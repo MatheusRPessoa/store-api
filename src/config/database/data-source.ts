52f2a3b1bf01da77ea8248d50b945133309266bf
Matheus Pessoa <matheus41.dev@gmail.com>
2026-03-17 15:16:43 -0300

    feat: imports order entity and order item entity in data source

--- src/config/database/data-source.ts
+++ src/config/database/data-source.ts
@@ -1,15 +1,17 @@
 // src/config/database/data-source.ts
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { ProductEntity } from '../../products/entities/product.entity';
-import { UserEntity } from '../../users/entities/user.entity';
+import { ProductEntity } from '../../modules/products/entities/product.entity';
+import { UserEntity } from '../../modules/users/entities/user.entity';
+import { OrderEntity } from '../../modules/orders/entities/order.entity';
+import { OrderItemEntity } from '../../modules/orders/entities/order-item.entity';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: 'database.sqlite',
   synchronize: true,
   logging: false,
-  entities: [UserEntity, ProductEntity],
+  entities: [UserEntity, ProductEntity, OrderEntity, OrderItemEntity],
   migrations: [],
   subscribers: [],
 });
