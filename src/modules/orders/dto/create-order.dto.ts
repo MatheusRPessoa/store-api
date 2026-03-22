ec535d40fc07f8c05b830fc787ce23c484368a0a
Matheus Pessoa <matheus41.dev@gmail.com>
2026-03-22 17:56:17 -0300

    fix: add examples in api property

--- src/modules/orders/dto/create-order.dto.ts
+++ src/modules/orders/dto/create-order.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
+  ArrayMinSize,
   IsArray,
   IsNotEmpty,
   IsNumber,
+  IsPositive,
   Min,
   ValidateNested,
 } from 'class-validator';
@@ -11,20 +13,25 @@ import {
 class CreateOrderItemDto {
   @ApiProperty({
     description: 'ID do produto',
-    example: '1234567',
+    example: 1234,
+    type: Number,
   })
   @IsNumber({}, { message: 'O ID do produto deve ser do tipo número' })
+  @IsPositive({ message: 'O ID do produto deve ser um número positivo' })
   @IsNotEmpty({ message: 'O ID do produto é obrigatório' })
   ID_PRODUTO: number;
 
   @ApiProperty({
     description: 'QUANTIDADE do produto no pedido',
     example: 2,
+    minimum: 1,
+    type: Number,
   })
   @IsNumber(
     {},
     { message: 'A QUANTIDADE do produto no pedido deve ser do tipo número' },
   )
+  @IsPositive({ message: 'A quantidade deve ser um número positivo' })
   @Min(1, { message: 'A quantidade mínima é 1' })
   QUANTIDADE: number;
 }
@@ -33,8 +40,23 @@ export class CreateOrderDto {
   @ApiProperty({
     description: 'Lista de items do pedido',
     type: [CreateOrderItemDto],
+    example: [
+      {
+        ID_PRODUTO: 1,
+        QUANTIDADE: 1,
+      },
+      {
+        ID_PRODUTO: 2,
+        QUANTIDADE: 1,
+      },
+      {
+        ID_PRODUTO: 3,
+        QUANTIDADE: 1,
+      },
+    ],
   })
   @IsArray({ message: 'Os itens devem ser um array' })
+  @ArrayMinSize(1, { message: 'O pedido deve conter pelo ,enos 1 item' })
   @ValidateNested({ each: true })
   @Type(() => CreateOrderItemDto)
   ITEMS: CreateOrderItemDto[];
