11db29431170c149c900efd6dd61c7db8a2e3396
Matheus Pessoa <matheus41.dev@gmail.com>
2026-03-22 17:50:25 -0300

    fix: swagger fix in the auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,25 +4,67 @@ import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
 import type { AuthenticatedRequest } from 'src/common/interface/authenticated-request.inteface';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LoginDto } from './dto/login.dto';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { LoginResponseDto } from './dto/login-response.dto';
+import { BaseSuccessResponseDto } from 'src/common/dto/pagination/base-response.dto';
+import { UnauthorizedResponseDto } from 'src/common/dto/pagination/error-response.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login(@Body() body: LoginDto) {
+  @ApiOperation({
+    summary: 'Realizar login',
+    description: 'Autentica o usuário e retorna tokens JWT',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Login efetuado com sucesso',
+    type: LoginResponseDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Credenciais inválidas',
+    type: LoginResponseDto,
+  })
+  async login(@Body() body: LoginDto) {
     return this.authService.login(body.username, body.password);
   }
 
   @UseGuards(JwtRefreshGuard)
   @Post('refresh')
-  refresh(@Req() req: AuthenticatedRequest) {
+  @ApiOperation({
+    summary: 'Renovar tokens',
+    description: 'Gera novos tokens usando o refresh token',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Tokens renovados com sucesso',
+    type: LoginResponseDto,
+  })
+  async refresh(@Req() req: AuthenticatedRequest) {
     return this.authService.refresh(req.user.sub, req.user.refreshToken!);
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('logout')
-  logout(@Req() req: AuthenticatedRequest) {
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Realizar logout',
+    description: 'Invalida o refresh token do usuário e encerra a sessão',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Logout realizado com sucesso',
+    type: BaseSuccessResponseDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Token inválido ou expirado',
+    type: UnauthorizedResponseDto,
+  })
+  async logout(@Req() req: AuthenticatedRequest) {
     return this.authService.logout(req.user.sub);
   }
 }
